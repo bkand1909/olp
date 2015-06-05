b7f40fc75dc04474db8b7169aed1c32893bf2717
Hung Le <hungle.info@gmail.com>
2015-06-05 19:10:37 +0700

    Add MapDemo.java

---
+++ src/sample/datastruce/MapDemo.java
@@ -0,0 +1,20 @@
+package sample.datastruce;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class MapDemo {
+
+	public static void main(String[] args) {
+		
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		map.put("first", 1);
+		map.put("second", 2);
+		map.put("first", 3);
+		for (Entry<String, Integer> pair : map.entrySet()) {
+			System.out.println(pair.getKey() + " " + pair.getValue());
+			System.out.println(map.get(pair.getKey()));
+		}
+	}
+}
